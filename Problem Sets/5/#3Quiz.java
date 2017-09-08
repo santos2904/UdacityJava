4e8f46992378533d4fe9f03dbee2fb00612a970b
Sriskan V <31234293+santos2904@users.noreply.github.com>
2017-09-08 17:54:23 -0400

    Create #3Quiz.java

---
+++ Problem Sets/5/#3Quiz.java
@@ -0,0 +1,49 @@
+//Create a class Word. Word has a constructor that takes a string argument
+//and one method getSubstrings which returns a String containing
+//all substring of word, sorted by length.
+
+//For example, if the user provides the input "rum", the method returns a
+//string that will print like this
+//r
+//u
+//m
+//ru
+//um
+//rum
+
+
+//You can concatenate the substrings in a String, separating them with a newline
+//("\n"). Then return the string
+
+// Need help starting this question? In the lesson titled 
+// "Starting points: Problem Set Questions", go to the
+// problem titled "Problem Set 5 - Question 3" for some tips on 
+// how to begin.
+
+public class Word
+{
+    private String word;
+	private int length;
+	
+		public Word (String word) //TODO write the constructor and define the instance variable
+	{
+		this.word = word;
+		this.length = word.length();
+	}
+    /**
+     * Gets all the substrings of this Word.
+     * @return all substrings of this Word separated by newline
+     */
+    public String getSubstrings()
+    {
+        String substring = "";
+        for (int i = 1; i <= length; i++)
+        {
+            for (int j = 0; i + j <= length; j++)
+            {
+                substring = substring + word.substring(j, i + j) + "\n";
+            }
+        }
+       return substring; //TODO implement this method
+    }
+}
