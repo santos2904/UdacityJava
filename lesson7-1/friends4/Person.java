19337cce708a8d4359d6431949644d16c924fa37
Santos V <santhos2904@gmail.com>
2017-12-13 18:53:15 -0500

    22. Quiz: Unfriend Revisted

--- lesson7-1/friends4/Person.java
+++ lesson7-1/friends4/Person.java
@@ -7,62 +7,58 @@
 
 import java.util.ArrayList;
 
-public class Person
-{
-    private String name;
-    private ArrayList<String> friends;
+public class Person {
 
-    public Person(String name)
-    {
+    private String name;
+    private ArrayList<Person> friends;
+
+    public Person(String name) {
         this.name = name;
-        friends = new ArrayList<String>();
+        friends = new ArrayList<Person>();
     }
 
     /**
      * Adds the given friend to this Person's friends list.
+     *
      * @param friend the friend to add.
      */
-    public void addFriend(Person friend)
-    {
-        friends.add(friend.name);
+    public void addFriend(Person friend) {
+        friends.add(friend);
     }
 
     /**
      * Gets a list of all of this Person's friends.
-     * @return the names of the friends separated by a comma and a space.
-     *     e.g. "Sara, Cheng-Han, Cay"
+     *
+     * @return the names of the friends separated by a comma and a space. e.g.
+     * "Sara, Cheng-Han, Cay"
      */
-    public String getFriends()
-    {
-        String friendsWithBrackets = friends.toString();
-        return friendsWithBrackets.substring(1, friendsWithBrackets.length() - 1);
+    public String getFriends() {
+        return getFriends(", ");
     }
 
     /**
      * Returns the names of the friends with separator between them.
+     *
      * @param separator string to put between names.
      * @return the names of the friends.
      */
-    public String getFriends(String separator)
-    {
+    public String getFriends(String separator) {
         String separatedFriends = "";
-        if (friends.size() > 0)
-        {
-            separatedFriends = separatedFriends + friends.get(0);
+        if (friends.size() > 0) {
+            separatedFriends = separatedFriends + friends.get(0).name;
         }
-        for (int i = 1; i < friends.size(); i++)
-        {
-            separatedFriends = separatedFriends + separator + friends.get(i);
+        for (int i = 1; i < friends.size(); i++) {
+            separatedFriends = separatedFriends + separator + friends.get(i).name;
         }
         return separatedFriends;
     }
 
     /**
      * Gets the number of friends this Person has.
+     *
      * @return the number of friends.
      */
-    public int getNumFriends()
-    {
+    public int getNumFriends() {
         return friends.size();
     }
 }
