38ea15d8ecdc4ec1c7391a38efa9fc1d59452544
Santos V <santhos2904@gmail.com>
2017-12-18 23:34:44 -0500

    Problem sets update 13

--- Problem Sets/6/Lesson 16. Problem Set 6.java
+++ Problem Sets/6/Lesson 16. Problem Set 6.java
@@ -137,6 +137,60 @@ public class AddTester
     }
 }
 
+12. // Complete this class. An array list of Strings called cities has been defined
+// for you and partially populated. Add code to do the following:
+// 1. Add the string "New York so that it is first in the arraylist
+// 2. replace the string "Dallas" (should be at index 2. Don't search) with "Houston"
+// 3. remove last element in the list
+// 4. print the arraylist
 
+import java.util.ArrayList;
 
+public class ArrayListMethodTester
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cities = new ArrayList<String>();
+        cities.add("Chicago");
+        cities.add("Dallas");
+        cities.add("San Jose");
+        cities.add("Seattle");
+
+        // TODO: Add code for steps 1-4 above
+        cities.add(0, "New York");
+        cities.remove("Dallas");
+        cities.add(2, "Houston");
+        cities.remove(cities.size() -1);
+
+        // TODO: Print the arraylist
+        System.out.println(cities);
+        System.out.println("Expected: [New York, Chicago, Houston, San Jose]");
+    }
+}
+
+13. // Complete this class. An array list of Strings called cities has been defined
+// for you and populated. You are to print the elements, one to a line (please 
+// use a for loop). Afterwards, print the element at index 2.
+
+import java.util.ArrayList;
+
+public class GetAndLoopDemo
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> cities = new ArrayList<String>();
+        cities.add("Chicago");
+        cities.add("Dallas");
+        cities.add("San Jose");
+        cities.add("Seattle");
+
+        // TODO: Use a loop to print the elements one to a line
+        for (String city: cities) {
+            System.out.println(city);
+        }
+        // TODO: Print the element at index 2
+        System.out.println(cities.get(2));
+
+    }
+}
 
