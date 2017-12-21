a5982ee611e69f4745f4b6b52f4300b9bb3eafef
Santos V <santhos2904@gmail.com>
2017-12-21 12:57:29 -0500

    Update 16

--- Problem Sets/6/Lesson 16. Problem Set 6.java
+++ Problem Sets/6/Lesson 16. Problem Set 6.java
@@ -250,3 +250,27 @@ public class StringArrayUtil
    }
 }
 
+16. // Complete the method in the class to print the elements in the array list in reverse order,
+// one to a line.
+// HINT: Use a for loop. Start with i = last index and work backwards.
+
+import java.util.ArrayList;
+
+public class ReversePrinter
+{
+    /**
+     * prints the array list in reverse order, one to a line
+     * @param text the array list to print
+     */
+     public void reversePrint(ArrayList<String> text)
+     {
+         // TODO: Write the code to print the elements in the array list in reverse order,
+         // one to a line.
+         int length = text.size();
+		 for (int i = length - 1; i >= 0; i--) {
+			 System.out.println(text.get(i));
+		 }
+	 }
+}
+
+
