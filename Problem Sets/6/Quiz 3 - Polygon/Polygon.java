45455d92277b7e95a88e63e765197cc0a6762e92
Santos V <santhos2904@gmail.com>
2018-01-01 17:15:28 -0500

    update

---
+++ Problem Sets/6/Quiz 3 - Polygon/Polygon.java
@@ -0,0 +1,21 @@
+//Create a Polygon class. A polygon is a closed shape with lines joining the corner points.
+//You will keep the points in an array list. Use object of java.awt.Point for the point.
+
+//Polygon will have as an instance variable an ArrayList of Points to hold the points
+//The constructor takes no parameters but initializes the instance variable.  The
+//Polygon class also has the following methods:
+//    add: adds a Point to the polygon
+//    perimeter: returns the perimeter of the polygon
+//    draw: draws the polygon by connecting consecutive points and then
+//          connecting the last point to the first.
+//
+//No methods headers or javadoc is provided this time. You get to try your hand at writing
+//a class almost from scratch
+
+import java.util.ArrayList;
+import java.awt.Point;
+
+public class Polygon
+{
+    // TODO: provide the required constructor, instance variable, and methods
+}
