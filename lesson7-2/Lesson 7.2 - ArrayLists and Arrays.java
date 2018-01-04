fdda1b30b2d802e15c0267b71a5003d21b258070
Santos V <santhos2904@gmail.com>
2018-01-04 15:27:07 -0500

    Lesson 7.1 - 1

---
+++ lesson7-2/Lesson 7.2 - ArrayLists and Arrays.java
@@ -0,0 +1,9 @@
+1. Arrays
+double [] values = new double [10];
+//double[] is the type
+//[10] is the length (cannot change)
+
+double[] moreValues = {32, 54, 67.5, 29, 35};
+//another way of constructing an array
+
+2. Array Elements
