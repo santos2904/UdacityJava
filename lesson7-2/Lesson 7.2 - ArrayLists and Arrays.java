280dba7ffa5c3ed93f5342357c11ea8855effc34
Santos V <santhos2904@gmail.com>
2018-01-04 15:35:23 -0500

    Lesson 7.1 - 4

--- lesson7-2/Lesson 7.2 - ArrayLists and Arrays.java
+++ lesson7-2/Lesson 7.2 - ArrayLists and Arrays.java
@@ -1,4 +1,4 @@
-1. Arrays
+//1. Arrays
 double [] values = new double [10];
 //double[] is the type
 //[10] is the length (cannot change)
@@ -6,7 +6,7 @@ double [] values = new double [10];
 double[] moreValues = {32, 54, 67.5, 29, 35};
 //another way of constructing an array
 
-2. Array Elements
+//2. Array Elements
 
 double firstValue = values[0];
 //[0] is the index
@@ -24,6 +24,15 @@ for (double value : values) {
 }
 // enchanced for loop
 
-3. Declare an Array Containing the First Five Primes in one line
+//3. Declare an Array Containing the First Five Primes in one line
 A: int prime[] = { 2, 3, 5, 7, 11 };
 
+//4. How does this loop Modify the Array?
+int[] primes = { 2, 3, 5, 7, 11 };
+
+for (int i = 0; i < 2; i++) {
+	primes [4 - i] = primes[i];
+}
+//What does primes contain after running this loop?
+
+A: 2, 3, 5, 3, 2
