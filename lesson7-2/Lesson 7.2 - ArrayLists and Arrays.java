a336f181be1cc110877c993c7d6a07a9849cf220
Santos V <santhos2904@gmail.com>
2018-01-04 15:32:24 -0500

    Lesson 7.1 - 3

--- lesson7-2/Lesson 7.2 - ArrayLists and Arrays.java
+++ lesson7-2/Lesson 7.2 - ArrayLists and Arrays.java
@@ -7,3 +7,23 @@ double[] moreValues = {32, 54, 67.5, 29, 35};
 //another way of constructing an array
 
 2. Array Elements
+
+double firstValue = values[0];
+//[0] is the index
+
+values[0] = 42
+//Storing value 42 to 0
+
+for (int i = 0; < values.length; i++) {
+	System.out.println(values[i]);
+}
+// for loop
+
+for (double value : values) {
+	System.out.println(values);
+}
+// enchanced for loop
+
+3. Declare an Array Containing the First Five Primes in one line
+A: int prime[] = { 2, 3, 5, 7, 11 };
+
