aa7b7a83ea1d5a1d0db5e5b854df21e57e2b4990
Santos V <santhos2904@gmail.com>
2018-01-10 23:50:21 -0500

    Problem set 7

---
+++ Problem Sets/7/Problem Set 7.java
@@ -0,0 +1,7 @@
+1. Write the code to declare an array of doubles called prices
+
+double[] prices;
+
+2. Complete the statement to create an array of ints called scores that can hold 50 ints
+
+int[] scores = new int[50];
