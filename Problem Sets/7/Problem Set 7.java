24572f6c3419831f9a8bd699ffc094acf437f53f
Santos V <santhos2904@gmail.com>
2018-01-10 23:57:47 -0500

    Problem set 7

--- Problem Sets/7/Problem Set 7.java
+++ Problem Sets/7/Problem Set 7.java
@@ -1,7 +1,24 @@
 1. Write the code to declare an array of doubles called prices
-
 double[] prices;
 
-2. Complete the statement to create an array of ints called scores that can hold 50 ints
 
+2. Complete the statement to create an array of ints called scores that can hold 50 ints
 int[] scores = new int[50];
+
+
+3. double[] temperatures = new double[12];
+What is the length of the array, that is how many elements can it hold?
+12
+
+
+4.  double[] temperatures = new double[12];
+What is the initial value at index 5? 
+0
+
+
+5. double[] temperatures = new double[12];
+Complete this code to print the element at index 9:
+System.out.println(temperatures[9]); 
+
+6. Write the statement to assign 95 to the first element of an int array scores
+scores[0] = 95;
