172fd9965a11b8d388700713832285c5a2d6535a
Santos V <santhos2904@gmail.com>
2018-01-18 14:23:38 -0500

    Quiz: Swapping Halves Finished

--- lesson7-3/swapHalves/SwapHalves.java
+++ lesson7-3/swapHalves/SwapHalves.java
@@ -1,14 +1,9 @@
 // BlueJ project: lesson7/swapHalves
 
-/**
-
-Reminder: Here is how you swap two elements
-
-int saved = elements[i];
-elements[i] = elements[j];
-elements[j] = saved;
-
-*/
+//Reminder: Here is how you swap two elements:
+//int saved = elements[i];
+//elements[i] = elements[j];
+//elements[j] = saved;
 
 import java.util.Arrays;
 
@@ -17,10 +12,16 @@ public class SwapHalves
     public static void main(String[] args)
     {
         int[] elements = { 1, 4, 9, 7, 3, 2, 5, 11, 0, 9 };
-
         // TODO: Swap the first and second half
-
+        int half = elements.length / 2;
+        for (int i = 0; i < half; i ++) {
+            int j = i + half;
+            int saved = elements[i];
+            elements[i] = elements[j];
+            elements[j] = saved;
+        }
         // Print all elements
         System.out.println(Arrays.toString(elements));
+		// Use Arrays.toString when printing all of the values 
     }
 }
