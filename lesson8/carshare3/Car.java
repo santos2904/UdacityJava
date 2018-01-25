390c82c356ba7d46eef0d3e95ff7131754bcda8f
Santos V <santhos2904@gmail.com>
2018-01-25 18:56:13 -0500

    Cars fix

--- lesson8/carshare3/Car.java
+++ lesson8/carshare3/Car.java
@@ -1,15 +1,15 @@
+
 import java.util.ArrayList;
 
-public class Car
-{
+public class Car {
+
     private ArrayList<Person> passengers;
     private int numberOfSeats;
     private String driverName;
     private int location;
     private int destination;
 
-    public Car(String driverName, int numberOfSeats, int destination)
-    {
+    public Car(String driverName, int numberOfSeats, int destination) {
         passengers = new ArrayList<Person>();
         this.driverName = driverName;
         this.numberOfSeats = numberOfSeats;
@@ -18,67 +18,58 @@ public class Car
     }
 
     /**
-       Gets the name of this car's driver.
-       @return the driver name
-    */
-    public String getDriverName()
-    {
+     * Gets the name of this car's driver.
+     *
+     * @return the driver name
+     */
+    public String getDriverName() {
         return driverName;
     }
 
     /**
-       Checks whether this car has arrived at its destination.
-       @return true if the car has arrived
-    */
-    public boolean hasArrived()
-    {
+     * Checks whether this car has arrived at its destination.
+     *
+     * @return true if the car has arrived
+     */
+    public boolean hasArrived() {
         return location == destination;
     }
 
     /**
-       Tries to add a passenger.
-       @param p the new passenger
-       @return true if there is room in the car and the passenger's
-       destination on the way to this car's destination
-    */
-    public boolean tryToAdd(Person p)
-    {
+     * Tries to add a passenger.
+     *
+     * @param p the new passenger
+     * @return true if there is room in the car and the passenger's destination
+     * on the way to this car's destination
+     */
+    public boolean tryToAdd(Person p) {
         if (passengers.size() + 2 <= numberOfSeats
-                && p.getDestination() <= destination)
-        {
+                && p.getDestination() <= destination) {
             passengers.add(p);
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
     /**
-       Drives this car to the next location and drops off
-       any passengers whose destination is that location
-    */
-    public void drive()
-    {
+     * Drives this car to the next location and drops off any passengers whose
+     * destination is that location
+     */
+    public void drive() {
         location++;
         int i = 0;
-        while (i < passengers.size())
-        {
+        while (i < passengers.size()) {
             Person p = passengers.get(i);
-            if (p.getDestination() == location)
-            {
+            if (p.getDestination() == location) {
                 passengers.remove(i);
-            }
-            else
-            {
+            } else {
                 i++;
             }
         }
     }
 
-    public String toString()
-    {
+    public String toString() {
         return "Car[location=" + location + ",driverName=" + driverName + ",passengers=" + passengers + "]";
     }
 }
